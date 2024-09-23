cbde539d2799607d45039c1511f30a5864cb8d9a
Eda AYDIN <edaaydin.ea@outlook.com>
2024-09-24 00:50:09 +0300

    Add littlekid.py to check if a person is Alice or a little kid

---
+++ Chapter2/littlekid.py
@@ -0,0 +1,11 @@
+name = "Carol"
+age = 3000
+if name == "Alice":
+    print("Hi, Alice.")
+elif age < 12:
+    print("You are not Alice, kiddo.")
+else:
+    print("You are neither Alice nor a little kid.")
+    
+    
+# The code will print the string "You are neither Alice nor a little kid." because the else clause is executed when none of the previous conditions are True. The else clause is a catch-all statement that runs when none of the conditions are True.
