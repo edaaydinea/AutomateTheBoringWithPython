f5c2f27af0a62e151a1008bb961b0e51a069f314
Eda AYDIN <edaaydin.ea@outlook.com>
2024-09-24 00:50:26 +0300

    Add yourName.py to prompt the user for their name and print a thank you message

---
+++ Chapter2/yourName.py
@@ -0,0 +1,8 @@
+name = ""
+while name != "your name":
+    print("Please type your name.")
+    name = input()
+print("Thank you!")
+
+
+# The code will keep asking the user to type their name until the user types "your name". Once the user types "your name", the program will print "Thank you!" and then end. The condition of the while loop is name != "your name", so the loop will continue to execute as long as the user has not typed "your name". The input() function call lets the user type in a string, which is stored in the name variable. The print() function call displays the string "Please type your name." to the user.
