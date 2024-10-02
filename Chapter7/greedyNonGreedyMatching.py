c291ed802a84028c622b748b896069dace86b37b
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-03 00:10:07 +0300

    Add script to demonstrate greedy and non-greedy matching with repetition operators

---
+++ Chapter7/greedyNonGreedyMatching.py
@@ -0,0 +1,9 @@
+import re
+
+greedyHaRegex = re.compile(r'(Ha){3,5}')
+mo1 = greedyHaRegex.search('HaHaHaHaHa')
+print(mo1.group()) # HaHaHaHaHa
+
+nongreedyHaRegex = re.compile(r'(Ha){3,5}?')
+mo2 = nongreedyHaRegex.search('HaHaHaHaHa')
+print(mo2.group()) # HaHaHa
