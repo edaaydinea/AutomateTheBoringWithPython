eee6637dca739d73b5417b4017ce5675c93601df
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-03 00:11:22 +0300

    Add script to demonstrate case-insensitive matching with regular expressions

---
+++ Chapter7/caseInsensitiveMatching.py
@@ -0,0 +1,10 @@
+import re
+
+regex1 = re.compile('RoboCop')
+regex2 = re.compile('ROBOCOP')
+regex3 = re.compile('robOcop')
+regex4 = re.compile('RobocOp')
+
+robocop = re.compile(r'robocop', re.I)
+print(robocop.search('RoboCop is part man, part machine, all cop.').group())
+print(robocop.search('ROBOCOP protects the innocent.').group())
